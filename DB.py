fba3368ca629fd99ea5d9e5ac5573186ec055a2d
Erik Samuelsson <erik.macallan.samuelsson@gmail.com>
2019-11-10 21:24:19 +0100

    Prettify.

--- DB.py
+++ DB.py
@@ -1,18 +1,23 @@
 from sqlalchemy import create_engine
 from config import PGURL
-import json 
-import re 
-from flask import abort 
+import json
+import re
+from flask import abort
+
 DB_URL = PGURL
 engine = create_engine(DB_URL, convert_unicode=True, echo=False)
 print(engine)
+
+
 class JsonRequestPackage:
+    def __init__(self, obj):
+        self.__dict__ = obj
 
-    def __init__(self, obj): self.__dict__ = obj
-        
+
 class TransactionObj:
-
-    def __init__(self, query_result = None, transaction_id = None, account_id = None, amount = None):
+    def __init__(
+        self, query_result=None, transaction_id=None, account_id=None, amount=None
+    ):
         if query_result == None:
             self.person_id = account_id
             self.balance = amount
@@ -22,35 +27,38 @@ class TransactionObj:
 
     def __repr__(self):
 
-        return "{"+f""" "person_id":"{self.person_id}", "balance": {self.balance} """+"}"
+        return (
+            "{"
+            + f""" "person_id":"{self.person_id}", "balance": {self.balance} """
+            + "}"
+        )
+
 
 class RequestHandler:
-
-    def __init__(self,request):
-        assert request.json != None 
+    def __init__(self, request):
+        assert request.json != None
         if request.json == None:
-            abort(406, 'Content-Type not json')
+            abort(406, "Content-Type not json")
         try:
-            self.payload =  JsonRequestPackage(request.json) 
+            self.payload = JsonRequestPackage(request.json)
             self.header = request.headers
-        except AttributeError: abort(406)
-        
-
+        except AttributeError:
+            abort(406)
 
     def __repr__(self):
         return f"""account_id:{self.payload.account_id}\namount:{self.payload.amount}"""
 
 
 class db:
-
     def __init__(self, engine):
         self.engine = engine
         self.connection = engine.connect()
 
-    def __contains__(self, obj):    
+    def __contains__(self, obj):
         return obj in self.result
 
-    def to_json(self): return json.dumps(json.loads(str(self.result)), indent= 4)
+    def to_json(self):
+        return json.dumps(json.loads(str(self.result)), indent=4)
 
     def get_balance(self, id_):
         q = f"""
@@ -61,33 +69,35 @@ class db:
                 )
                 select person_id, sum(amount) as balance from transactions group by person_id;
             """
-        
+
         self.result = TransactionObj(query_result=self.connection.execute(q).fetchone())
 
-        
-
-    def submit_amount(self, req): 
-        Table = 'Incomes' if req.payload.amount >= 0 else 'Expenses'
+    def submit_amount(self, req):
+        Table = "Incomes" if req.payload.amount >= 0 else "Expenses"
         q = f"""
                 insert into {Table} (id, person_id, amount) values(
                     '{req.header['Transaction-Id']}', '{req.payload.account_id}', '{req.payload.amount}'
                 ); 
             """
-        try: self.connection.execute(q)
-        except Exception as e: 
-            if 'duplicate key' in re.findall('duplicate key', str(e)):
-                abort(409, f'Attempted insertion of duplicate primary key into database.\n{e}')
-            else: abort(404, 'Submission could not be performed')
-
+        try:
+            self.connection.execute(q)
+        except Exception as e:
+            if "duplicate key" in re.findall("duplicate key", str(e)):
+                abort(
+                    409,
+                    f"Attempted insertion of duplicate primary key into database.\n{e}",
+                )
+            else:
+                abort(404, "Submission could not be performed")
 
     def person_exists(self, id_):
         q = f"select id, name from Persons where id='{id_}'"
         self.result = self.connection.execute(q).fetchone()
-        try: return id_ in self.result[0]
-        except TypeError: return False
+        try:
+            return id_ in self.result[0]
+        except TypeError:
+            return False
 
-    
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
--- app.py
+++ app.py
@@ -1,53 +1,41 @@
 from collections import namedtuple
-from flask import Flask, jsonify, request, render_template, abort 
-import json, os 
-from flask_sqlalchemy  import SQLAlchemy as sql
+from flask import Flask, jsonify, request, render_template, abort
+import json, os
+from flask_sqlalchemy import SQLAlchemy as sql
 from sqlalchemy import create_engine
 from config import PGURL
-from DB import db,  RequestHandler
+from DB import db, RequestHandler
 
 DB_URL = PGURL
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 engine = create_engine(DB_URL, convert_unicode=True, echo=False)
 
 
-
-
-
-
-
-    
 @app.route("/balance/<id_>", methods=["GET"])
 def balance(id_):
     db_ = db(engine)
-    if  db_.person_exists(id_) == False:
+    if db_.person_exists(id_) == False:
         abort(404)
     db_.get_balance(id_)
     return db_.to_json()
 
 
-@app.route('/amount', methods=['GET','POST'])
+@app.route("/amount", methods=["GET", "POST"])
 def insertion():
     req = RequestHandler(request)
     db_ = db(engine)
     exists = db_.person_exists(req.payload.account_id)
-    if not exists: 
-        abort(404, 'User does not exists.') 
+    if not exists:
+        abort(404, "User does not exists.")
     if type(req.payload.amount) == type(1) or type(req.payload.amount) == type(1.1):
         pass
     else:
         raise ValueError
     db_.submit_amount(req)
-        
 
-    return ''
-    
-
-
+    return ""
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True, port = 8080, passthrough_errors=True)
-
+    app.run(debug=True, port=8080, passthrough_errors=True)
